b67dfdaeb5eeecf7912aaa1663c1d604feaa0602
NAW0510 <132590419+NAW0510@users.noreply.github.com>
2025-04-04 03:06:20 +0800

    Add files via upload

---
+++ praktic/praktic/inheritance/mains/main.java
@@ -0,0 +1,72 @@
+package mains;
+
+import transportation.Bus;
+import transportation.Taxi;
+import transportation.Train;
+
+public class main {
+    public static void main(String[] args) { 
+        Bus bus = new Bus("Bus Transjakarta", 50, "Terminal A - Terminal B");
+        Train train = new Train("Kereta Commuter Line", 200, "Stasiun Kota - Stasiun Pusat");
+        Taxi taxi = new Taxi("Taksi Bluebird", 4, "Bandara - Hotel");
+
+        bus.startJourney();
+        bus.boardPassenger(30);
+        bus.boardPassenger(20);
+        bus.calculateFare(5);
+        bus.stopJourney();
+        bus.dropPassenger(50);
+        
+System.out.println("---------------------------------");
+
+        train.startJourney();
+        train.boardPassenger(150);
+        train.calculateFare();
+        train.stopJourney();
+        train.dropPassenger(150);
+
+System.out.println("---------------------------------");
+
+        taxi.startJourney();
+        taxi.boardPassenger(3);
+        System.out.println("Total penumpang Taksi: " + taxi.getCurrentPassengers() + " penumpang.");
+        taxi.calculateFare(10);
+        taxi.stopJourney();
+        taxi.dropPassenger(3);
+
+System.out.println("---------------------------------");
+
+        bus.startJourney();
+        bus.boardPassenger(30);
+        bus.boardPassenger(100);
+        bus.calculateFare(5);
+        bus.stopJourney();
+        bus.dropPassenger(30);
+        bus.dropPassenger(50); 
+
+
+System.out.println("---------------------------------");
+
+        train.startJourney();
+        train.boardPassenger(150);
+        train.boardPassenger(300);
+
+        train.calculateFare();
+        train.stopJourney();
+        train.dropPassenger(150);
+        train.dropPassenger(150);
+
+
+System.out.println("---------------------------------");
+
+        taxi.startJourney();
+        taxi.boardPassenger(3);
+        taxi.boardPassenger(10);
+        System.out.println("Total penumpang Taksi: " + taxi.getCurrentPassengers() + " penumpang.");
+        taxi.calculateFare(10);
+        taxi.stopJourney();
+        taxi.dropPassenger(3);
+        taxi.dropPassenger(3);
+
+    }
+}
---
+++ praktic/praktic/inheritance/transportation/Bus.java
@@ -0,0 +1,18 @@
+package transportation;
+public class Bus extends Vehicle {
+    private static final int FARE_PER_STOP = 2000;
+    
+    public Bus(String name, int capacity, String route) {
+        super(name, capacity, route);
+    }
+    public void calculateFare(int stops) {
+        int tarif = FARE_PER_STOP;
+        int fare = getCurrentPassengers()*stops * FARE_PER_STOP;
+        System.out.println("Total Penumpang: " + getCurrentPassengers());
+        System.out.println("Total Pemberhentian: " + stops);
+        System.out.println("Tarif per Halte: Rp " + tarif);
+        System.out.println("Total tarif perjalanan bus: Rp " + fare);
+    }
+    
+
+}
